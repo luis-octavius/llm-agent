42e5ada2f314ddabea14ef1eabd503bc21155c82
luis-octavius <luisoctavius.sc@gmail.com>
2025-08-12 19:11:47 -0300

    feat: add genai client

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import os
+from google import genai 
 from dotenv import load_dotenv
 
 def main():
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    )
+
+    print(response.text)
+    
+    metadata = response.usage_metadata
+
+    print(f"Prompt tokens: {metadata.prompt_token_count}")
+    print(f"Response tokens: {metadata.candidates_token_count}")
+
+
 
 if __name__ == "__main__":
     main()
