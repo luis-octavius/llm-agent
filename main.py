f797b7c89401cb757c2fd448749fded86458ea48
luis-octavius <luisoctavius.sc@gmail.com>
2025-08-12 19:21:43 -0300

    fix: delete hardcoded prompt to use argv from execution

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
+import sys 
 from google import genai 
 from dotenv import load_dotenv
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: main.py 'your_prompt here'")
+        sys.exit(1)
+
+    answer = sys.argv[1]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.0-flash-001', contents=answer
     )
 
     print(response.text)
