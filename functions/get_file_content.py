6e48974438f521fb7cbca1f0f975a696f90c70f0
luis-octavius <luisoctavius.sc@gmail.com>
2025-08-14 20:04:55 -0300

    refactor: cut some spaces

---
+++ functions/get_file_content.py
@@ -0,0 +1,38 @@
+import os
+
+
+def get_file_content(working_directory, file_path):
+    MAX_CHARS = 10000
+    joined = os.path.abspath(os.path.join(working_directory, file_path))
+    
+    absolute_curr_path = os.path.abspath(working_directory)
+    print("Joined: ", joined)
+  
+    print("Absolute Path Working Directory: ", absolute_curr_path)
+    is_file_within = joined.startswith(absolute_curr_path) 
+    print("Is file within: ", is_file_within)
+
+    if is_file_within == False:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if os.path.isfile(joined) == False:
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    print("Is file: ", os.path.isfile(joined))
+
+    with open(joined, "r") as f:
+        content = f.read(MAX_CHARS)
+
+        next_char = f.read(1)
+        if next_char:
+            content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        return content 
+        f.close()
+
+
+
+
+
+
+
