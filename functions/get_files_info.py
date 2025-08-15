6989a0e7c475e04733e4fc36e54a267f14a73777
luis-octavius <luisoctavius.sc@gmail.com>
2025-08-14 22:38:03 -0300

    feat: schema that makes agent list directories

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
 
@@ -40,5 +41,16 @@ def get_dir_content(dir):
 
     return "\n".join(files_list)
 
-
-
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+    "directory": types.Schema(
+    type=types.Type.STRING,
+    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
