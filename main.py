e643e95a961b8e48f5573186b6ee467c9c26a7a9
luis-octavius <luisoctavius.sc@gmail.com>
2025-08-16 17:08:51 -0300

    feat: the agent can read, list and write to specified files

--- main.py
+++ main.py
@@ -8,21 +8,8 @@ from functions.run_python_file import schema_run_python_file, run_python_file
 from functions.get_file_content import schema_get_file_content, get_file_content 
 from functions.write_file import schema_write_file, write_file 
 
-def main():
-    # check if there is a prompt message (string) in cli call 
-    if len(sys.argv) < 2:
-        print("Usage: main.py 'your_prompt here'")
-        sys.exit(1)
 
-    print("Sys argvs: ", sys.argv)
-    user_prompt = sys.argv[1]
-
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-
-    client = genai.Client(api_key=api_key)
-
-    system_prompt = """
+system_prompt = """
 You are a helpful AI coding agent.
 
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
@@ -34,73 +21,115 @@ When a user asks a question or makes a request, make a function call plan. You c
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
-    
-    # available functions to the use of agent
-    availabe_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,
-            schema_run_python_file,
-            schema_get_file_content,
-            schema_write_file,
-        ]
-    )
 
-    # use the prompt in messages property 
+availabe_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_run_python_file,
+        schema_get_file_content,
+        schema_write_file,
+    ]
+)
+
+MAX_ITERS = 20
+
+    
+def main():
+    # check if there is a prompt message (string) in cli call 
+    if len(sys.argv) < 2:
+        print("Usage: main.py 'your_prompt here'")
+        sys.exit(1)
+
+    verbose = "--verbose" in sys.argv 
+
+    print("Sys argvs: ", sys.argv)
+    user_prompt = sys.argv[1]
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
     messages = [
         types.Content(
         role="user", 
         parts=[types.Part(text=user_prompt)]
         ),
     ]
+    
+    iters = 0 
+    while True: 
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations reached. exiting...")
+            sys.exit(1)
 
-    # generate the response, give the instructions and specify the model 
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                    print("Final response: ")
+                    print(final_response)
+                    break
+        except Exception as e:
+            print(f"Error in generate content: {e}")
+
+    
+# generate the response, give the instructions and specify the model 
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
         contents=messages, 
-        config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[availabe_functions]), 
+        config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[availabe_functions]
+        ),
     )
-    
-    # check if cli call was made with verbose flag 
-    if "--verbose" in sys.argv:
-        verbose = True 
 
-    text = response.text
+    if verbose: 
+        print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+        print("Response tokens: ", response.usage_metadata.candidates_token_count)
 
-    # return a list with the function calls
-    calls = response.function_calls
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content 
+            messages.append(function_call_content)
 
-    if calls != None:
-        try:
-            for function in calls:
-                content = call_function(function, verbose)
-                function_call_result = content.parts[0].function_response.response 
-                if function_call_result and verbose:
-                    print(f"-> {function_call_result}")
-        except Exception as e:
-            raise Exception(f'Fatal error: {e}')
-    else:
-        print(text)
-    metadata = response.usage_metadata
-    
-    # if the cli call is with the flag verbose print some statistics
-    if verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {metadata.prompt_token_count}")
-        print(f"Response tokens: {metadata.candidates_token_count}")
+    if not response.function_calls:
+        return response.text 
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+
+        if (not function_call_result.parts or not function_call_result.parts[0].function_response):
+            raise Exception("empty function call result")
+
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+            function_responses.append(function_call_result.parts[0])
    
+    if not function_responses: # check if list is empty and thrown an Exception
+        raise Exception("no function responses generated, exiting.")
+
+    messages.append(types.Content( # generate the dialog with the agent
+        role="user",
+        parts=function_responses 
+    ))
 
 # responsible to call the functions in availabe functions 
 def call_function(function_call_part, verbose=False): # function_call_part is a types.FunctionCall
+    
+    function_name = function_call_part.name 
+    function_args = function_call_part.args 
+    if verbose:
+        print(f"Calling function: {function_name}({function_args})")
+    else:
+        print(f" - Calling function: {function_name}")
+    
     function_map = {
         "get_files_info": get_files_info, 
         "get_file_content": get_file_content,
         "write_file": write_file,
         "run_python_file": run_python_file,
     }
-
-    function_name = function_call_part.name 
-    function_args = function_call_part.args 
-    print(function_args)
 
     if function_name not in function_map:
         return types.Content(
@@ -113,11 +142,7 @@ def call_function(function_call_part, verbose=False): # function_call_part is a 
         ],
     )
 
-    if verbose == True:
-        print(f"Calling function: {function_name}({function_args})")
-    else:
-        print(f" - Calling function: {function_name}")
-    
+   
     args = dict(function_args)
     print("Args dict: ", args)
     args["working_directory"] = "./calculator"
